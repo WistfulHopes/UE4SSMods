a3a3ddc777e243e8dcb697e5042fcb75a2ee1a9f
Ryn <kp72002@gmail.com>
2024-05-18 13:43:52 -0700

    updated to latest ue4ss main

--- GBVSRollback/xmake.lua
+++ GBVSRollback/xmake.lua
@@ -1,9 +1,7 @@
 local projectName = "GBVSRollback"
 
 target(projectName)
-    set_kind("shared")
-    set_languages("cxx20")
-    set_exceptions("cxx")
+    add_rules("ue4ss.mod")
 
     add_includedirs("include", { public = true })
     add_headerfiles("include/**.hpp")
@@ -12,22 +10,3 @@ target(projectName)
     add_files("src/PatternFinder.cpp")
     add_files("src/BattleState.cpp")
     add_files("src/Particles.cpp")
-
-    add_deps(
-        "UE4SS"
-    )
-    
-    on_load(function (target)
-        import("build_configs", { rootdir = get_config("scriptsRoot") })
-        build_configs:set_output_dir(target)
-    end)
-    
-    on_config(function (target)
-        import("build_configs", { rootdir = get_config("scriptsRoot") })
-        build_configs:config(target)
-    end)
-    
-    after_clean(function (target)
-        import("build_configs", { rootdir = get_config("scriptsRoot") })
-        build_configs:clean_output_dir(target)
-    end)
--- ModuleManagerMod/xmake.lua
+++ ModuleManagerMod/xmake.lua
@@ -3,9 +3,7 @@ local projectName = "ModuleManagerMod"
 add_requires("raw_pdb", { debug = is_mode_debug(), configs = { runtimes = get_mode_runtimes() } })
 
 target(projectName)
-    set_kind("shared")
-    set_languages("cxx20")
-    set_exceptions("cxx")
+    add_rules("ue4ss.mod")
 
     add_includedirs("include", { public = true })
     add_headerfiles("include/**.hpp")
@@ -13,24 +11,4 @@ target(projectName)
     add_files("src/ModuleManager.cpp")
     add_files("src/SymbolResolver.cpp")
 
-    add_deps(
-        "UE4SS"
-    )
-
     add_packages("raw_pdb")
-
-    
-    on_load(function (target)
-        import("build_configs", { rootdir = get_config("scriptsRoot") })
-        build_configs:set_output_dir(target)
-    end)
-    
-    on_config(function (target)
-        import("build_configs", { rootdir = get_config("scriptsRoot") })
-        build_configs:config(target)
-    end)
-    
-    after_clean(function (target)
-        import("build_configs", { rootdir = get_config("scriptsRoot") })
-        build_configs:clean_output_dir(target)
-    end)
